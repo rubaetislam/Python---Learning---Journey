8648806096dc300446dc38269c4c498e32adcd12
rubaetislam <rubaetislam2326@gmail.com>
2026-07-13 13:25:42 +0600

    Add Day01 and Day02 code

---
+++ Day01/day01.py
@@ -0,0 +1,24 @@
+#Practice Day 01
+print("Hello Python")
+print("I want to become a Python Freelancer")
+Name = "RUBAET"
+Age = 20
+University = "Daffodil International University"
+print(Name)
+print(Age)
+print(University)
+country = "Bangladesh"
+cgpa = 3.85
+student = True
+print(country)
+print(cgpa)
+print(student)
+print(type(Name))
+print(type(cgpa))
+print(type(student))
+print(type(Age))
+#Homework Day 01
+print("My name is Rubaet Islam")
+print("I am 20 years old")
+print("I study Software Engineering at DIU")
+print("My goal is to become a Python Freelancer")
